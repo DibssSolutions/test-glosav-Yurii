59b89ebf62fe49ed80458892a5dbe2973b36d07d
YuriiFetko <fetkoyura@gmail.com>
2019-02-20 17:52:24 +0200

    20.02.19

--- src/js/components/_modalSlider.js
+++ src/js/components/_modalSlider.js
@@ -3,20 +3,25 @@ $(function() {
 
   modalContent.each(function(index, elem) {
     const itemsCount = $(elem).find('.items').length;
-    $(elem).attr('date-items', itemsCount);
+    $(elem).attr('data-items', itemsCount);
   });
-  
+  $('.sign-in-btn').click(function() {
+    $('.modalWindow').toggleClass('modalLogin');
+  });
+  $(document).on('click', function(hideLang) {
 
+  }
+  );
   var modal ={
     self: $('#modalWindow'),
     
 
     showModal:function(content) {
       this.self.find('#innerModal').html(content);
-      this.self.fadeIn(200);
+      this.self.fadeIn(500);
     },
     hideModal: function() {
-      this.self.fadeOut(200);
+      this.self.fadeOut(500);
       this.self.find('#innerModal').html('');
     }
   };
@@ -26,8 +31,9 @@ $(function() {
     modal.showModal(content);
   });
   $('#modalWindow').on('click',function(e) {
-    if ($(e.target).attr('id')==='modalWindow'|| $(e.target).hasClass('close-button-modal')) {
+    if ($(e.target).attr('id')==='modalWindow'|| $(e.target).hasClass('close-button-modal')|| $(e.target).hasClass('close') || $(e.target).hasClass('content-modal')) {
       modal.hideModal();
+      $('.modalWindow').removeClass('modalLogin');
     } else{
       return false;
     }
@@ -38,5 +44,7 @@ const modalContent = $('.modalContent');
 
 modalContent.each(function(index, elem) {
   const itemsCount = $(elem).find('.items').length;
-  $(elem).attr('date-items', itemsCount);
+  $(elem).attr('data-items', itemsCount);
 });
+
+
