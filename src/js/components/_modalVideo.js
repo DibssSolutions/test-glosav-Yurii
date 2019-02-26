2b92c886ee91f4f528812785e9d81bd210dbab39
YuriiFetko <fetkoyura@gmail.com>
2019-02-26 17:58:24 +0200

    26.02.19

--- src/js/components/_modalVideo.js
+++ src/js/components/_modalVideo.js
@@ -1,35 +1,35 @@
-let modal = document.querySelector('#modal'),
-  closeButton = document.querySelector('#close-button'),
-  openButton = document.querySelector('#open-button');
+// let modal = document.querySelector('#modal'),
+//   closeButton = document.querySelector('#close-button'),
+//   openButton = document.querySelector('#open-button');
   
-closeButton.addEventListener('click', function() {
-  modal.classList.toggle('closed');
-});
+// closeButton.addEventListener('click', function() {
+//   modal.classList.toggle('closed');
+// });
 
-const range = document.getElementById('scrollVideo');
+// const range = document.getElementById('scrollVideo');
 
-range.addEventListener('change', function(range) {
+// range.addEventListener('change', function(range) {
 
 
-  const value = range.target.value;
-  const maxValue = 100;
-  if (+value === maxValue) {
-    $('.carousel-wrap').fadeOut(100);
-    $('.modal').fadeIn(800);
-    $('#close-button').click(function() {
-      $('.modal').fadeOut(500);
-      $('.carousel-wrap').fadeIn(1000);
-    });
-    modal.classList.toggle('closed');
-    document.getElementById('close-button').onclick = function(e) {
-      document.getElementById('scrollVideo').value = '0';
-    };
-  }
+//   const value = range.target.value;
+//   const maxValue = 100;
+//   if (+value === maxValue) {
+//     $('.carousel-wrap').fadeOut(100);
+//     $('.modal').fadeIn(800);
+//     $('#close-button').click(function() {
+//       $('.modal').fadeOut(500);
+//       $('.carousel-wrap').fadeIn(1000);
+//     });
+//     modal.classList.toggle('closed');
+//     document.getElementById('close-button').onclick = function(e) {
+//       // document.getElementById('scrollVideo').value = '0';
+//     };
+//   }
 
-  if(+value !== maxValue) {
-    document.getElementById('scrollVideo').value = '0';
-  }
-});
+//   if(+value !== maxValue) {
+//     document.getElementById('scrollVideo').value = '0';
+//   }
+// });
 
 // jQuery(function($) {
 //   $(document).mouseup(function(e) { // событие клика по веб-документу
