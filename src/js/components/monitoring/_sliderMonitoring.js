7c84b862fdc6a6337164be0205ae16c9bbba6ea6
YuriiFetko <fetkoyura@gmail.com>
2019-03-01 16:52:55 +0200

    01.03.19

--- src/js/components/monitoring/_sliderMonitoring.js
+++ src/js/components/monitoring/_sliderMonitoring.js
@@ -1,11 +1,12 @@
 import slick from 'slick-carousel';
 
-$('.js-monitoring-slider').slick({
-//   dots: true,
+var settings = {
   infinite: true,
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
   responsive: [
     {
       breakpoint: 1024,
@@ -30,8 +31,36 @@ $('.js-monitoring-slider').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
+};
+var slickIsActive;
+$().ready(function() {
+  $('.js-monitoring-slider').slick(settings);
 });
+
+var slickIsActive;
+
+$('.js-monitoring-slider').on('init', function() {
+  slickIsActive = true;
+});
+
+function toogleSlick() {  
+  if (slickIsActive) {
+    $('.js-monitoring-slider').slick('unslick');
+    slickIsActive = false;
+    $('.js-monitoring-slider').toggleClass('row').css('justify-content', 'center');
+    $('.js-col').toggleClass('col-3').css('padding-bottom', '38px');
+    $('.monitoring-slider__btn').css('margin-top', '0px');
+    $('.slider-arrows').toggleClass('none');
+    $('.monitoring-slider__btn').html('Скрыть');
+  } else {
+    $('.js-monitoring-slider').slick(settings);
+    $('.js-monitoring-slider').removeClass('row');
+    $('.js-col').removeClass('col-3');
+    $('.slider-arrows').removeClass('none');
+    $('.monitoring-slider__btn').html('Смотреть все решения');
+  }
+}
+
+$('.monitoring-slider__btn').on('click', toogleSlick);
+
