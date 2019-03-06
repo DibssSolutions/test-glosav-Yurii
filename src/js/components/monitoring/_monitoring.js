e0a89e632f559b5657e8caf2ba5150258be72792
YuriiFetko <fetkoyura@gmail.com>
2019-03-06 18:12:35 +0200

    06.03

--- src/js/components/monitoring/_monitoring.js
+++ src/js/components/monitoring/_monitoring.js
@@ -2,6 +2,8 @@
 // $( 'body' ).find( '.out' ).css( 'background-color', 'red' );
 if (document.querySelector('.monitoring')) {
   $('.out').css('background', 'none');
+  $('body').css('overflow', 'auto');
+  $('footer').css('background', '#dee6eb');
 }
 $('.hero-ask__button').click(function() {
   $( '.hero-ask__desc p:not(:first)' ).toggleClass('d-block').css('padding-top','30px');
--- src/js/components/monitoring/_select.js
+++ src/js/components/monitoring/_select.js
@@ -1,7 +1,21 @@
-import 'select2';
 
+import select2 from 'select2';
 
-$('.js-select').select2({
-  placeholder: 'Select a state',
+// var select = $('.js-select');
 
+// select.select2({
+// });
+
+// var selectNews = $('.js-select-news');
+
+// selectNews.select2({
+//   minimumResultsForSearch: Infinity,
+//   dropdownParent: $('.news__dropdown'),
+//   placeholder: 'Новости по всем направлениям'
+// });
+
+jQuery(document).ready(function($) {
+  $('.js-select').select2({width:100});
+  $('b[role="presentation"]').hide();
+  $('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');
 });
